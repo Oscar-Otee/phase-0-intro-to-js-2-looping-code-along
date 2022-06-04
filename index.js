e30b2117425476b4f88ec2b86ef0d666651cbab4
oscar-otee <oluoch.oscar.oo@gmail.com>
2022-06-04 19:55:31 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames, eventName) {
+    arrayOfMessages = [];
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        debugger;
+        arrayOfMessages.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arrayOfMessages;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i--);
+    }
+
+}
+
+countDown(10);
